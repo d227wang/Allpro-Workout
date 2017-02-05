6cb1010e4f4a464134bb40b26c7c56baa53db64f
Daren Wang <d227wang@uwaterloo.ca>
2017-02-05 15:38:26 -0700

    Set up log view

---
+++ AllPro Workout/LogViewController.h
@@ -0,0 +1,14 @@
+//
+//  LogViewController.h
+//  AllPro Workout
+//
+//  Created by Daren on 2017-02-05.
+//  Copyright © 2017 dwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface LogViewController : UITableViewController
+
+@end
---
+++ AllPro Workout/LogViewController.m
@@ -0,0 +1,18 @@
+//
+//  LogViewController.m
+//  AllPro Workout
+//
+//  Created by Daren on 2017-02-05.
+//  Copyright © 2017 dwang. All rights reserved.
+//
+
+#import "LogViewController.h"
+
+@implementation LogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+@end
